985b421a9f5826965ee4f8bb789ed1b8bf7e3421
BG1supernode333 <bg.supernode@gmail.com>
2014-11-12 11:26:39 +0900

    メニュー表示の有効化

--- src/ObjeqtNote/ObjeqtNote/MainApplication.cpp
+++ src/ObjeqtNote/ObjeqtNote/MainApplication.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include "Resource.h"
 #include "MainApplication.h"
 #include "MainWindow.h"
 
@@ -10,7 +11,7 @@ BOOL CMainApplication::InitInstance(HINSTANCE hInstance){
 		LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION)),
 		LoadCursor(hInstance, IDC_ARROW),
 		(HBRUSH)GetStockObject(WHITE_BRUSH),
-		NULL
+		MAKEINTRESOURCE(IDC_OBJEQTNOTE)
 	);
 
 	BOOL bRet = pMainWnd->Create(_T("Main"), WS_OVERLAPPEDWINDOW | WS_HSCROLL | WS_VSCROLL, 0, 0, 639, 479, hInstance);
